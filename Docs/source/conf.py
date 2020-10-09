89e0c6133280bb9ada6af065ea85c38c92faa526
Jeff Sale <jsale@users.noreply.github.com>
2020-10-09 08:33:16 -0700

    Update conf.py

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -37,7 +37,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 #html_theme = 'nature'
-html_theme = 'alabaster'
+html_theme = 'nature'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
